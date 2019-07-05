2d0a21913427d9516adf38113a6bd07758ec46d0
Flávio Juvenal <flaviojuvenal@gmail.com>
2019-07-05 17:14:16 -0300

    Add MAX_RESULTS to ZomboDBAdminMixin

--- django_zombodb/admin_mixins.py
+++ django_zombodb/admin_mixins.py
@@ -8,6 +8,7 @@ from django_zombodb.helpers import validate_query_string
 
 
 class ZomboDBAdminMixin:
+    MAX_RESULTS = None
 
     def get_search_fields(self, request):
         """
@@ -39,7 +40,11 @@ class ZomboDBAdminMixin:
         if search_term:
             if request._has_valid_search:
                 queryset = queryset.query_string_search(
-                    search_term, validate=False, sort=False, score_attr='zombodb_score'
+                    search_term,
+                    validate=False,
+                    sort=False,
+                    score_attr='zombodb_score',
+                    limit=self.MAX_RESULTS
                 ).annotate_score()
             else:
                 self.message_user(
